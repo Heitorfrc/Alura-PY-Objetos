5c97cb0e00491db008b4d5be7c492fee48686949
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-06-27 17:18:48 -0300

    Create introducao.py

---
+++ introducao.py
@@ -0,0 +1,9 @@
+'''começando o curso de orientação a objetos'''
+
+pais = "Italia"
+quantidade = 4
+
+print(type(pais))
+print(type(quantidade))
+
+print(pais, "ganhou", quantidade, "titulos mundiais")
