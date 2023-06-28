db5f1bcda542ac55f5221712e4ee493d03d2e1e2
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-06-28 16:06:10 -0300

    Add files via upload

---
+++ adivinhacao.py
@@ -0,0 +1,21 @@
+''' Jogo de Adivinhação '''
+
+print("#################################")
+print("Bem vindo ao jogo de Adivinhação!")
+print("#################################")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+chute = int(chute_str)
+
+print("Você digitou", chute)
+
+if(numero_secreto == chute) :
+    print("Você acertou")
+else :
+    print("Você errou")
+
+print("Fim do jogo")
+
+print(type(chute))
