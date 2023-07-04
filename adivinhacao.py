4fb0552c440353e52ae8e2a5ad65a9f20f079ad3
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-07-04 18:50:29 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,13 +6,17 @@ print("#################################")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas) :
-    print("Tentativa", rodada, "de", total_de_tentativas)
-    chute_str = input("Digite o seu número: ")
+
+for rodada in range(1, total_de_tentativas + 1) :
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
     chute = int(chute_str)
     print("Você digitou", chute)
+
+    if(chute < 1 or chute > 100) :
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -20,14 +24,15 @@ while(rodada <= total_de_tentativas) :
 
     if(acertou) :
         print("Você acertou!!")
-    else :
+        break
+    else : 
         if(maior) :
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif(menor) :
             print("Você errou! O seu chute foi menor que o número secreto.")
 
-    rodada += 1
+    
 
 print("Fim do jogo")
 
-print(type(acertou))
+print(type(chute_str))
