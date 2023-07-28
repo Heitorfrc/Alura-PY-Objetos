82871bfd59c29ceb683cbf1b7093c48f7b25e15a
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-07-28 16:25:44 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -6,27 +6,37 @@ def jogar() :
     print("## Bem vindo ao jogo da Forca! ##")
     print("#################################")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "bola".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou) :
 
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
-        index = 0
-        for letra in palavra_secreta :
-            if(chute.upper == letra.upper) :
-                letras_acertadas[index] = letra
-            index += 1
+        if(chute in palavra_secreta) :
+            index = 0
+            for letra in palavra_secreta :
+                if(chute == letra) :
+                    letras_acertadas[index] = letra
+                index += 1
+        else :
+            erros += 1
         
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou) :
+        print("Parabéns você acertou a palavra!")
+    else :
+        print("Forca!")
 
     print("Fim do jogo")
 
