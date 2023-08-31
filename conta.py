f476417c46be90d7135afaa29b052c23dccad3ee
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-08-31 17:16:42 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -4,19 +4,23 @@ class Conta :
 
     def __init__(self, numero, titular, saldo, limite) -> None:
         print("Construindo Objeto ...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self) :
-        print("Saldo de {} reais do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} reais do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor) :
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor) :
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino) :
+        self.saca(valor)
+        destino.deposita(valor)       
 
 
 
@@ -24,8 +28,10 @@ conta = Conta(123, "Heitor", 55.0, 1000.0)
 
 conta2 = Conta(321, "Marco", 100.0, 1000.0)
 
-conta.saca(15.0)
+conta.transfere(15.0, conta2)
 
 conta.extrato()
 
-print(conta.titular)
+conta2.extrato()
+
+print(conta._Conta__titular)
