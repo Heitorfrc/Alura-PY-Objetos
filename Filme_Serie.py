94f99b37c95b8ebd851cd3f745d77646f226ebe1
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-10-04 18:43:28 -0300

    Update Filme_Serie.py

--- Filme_Serie.py
+++ Filme_Serie.py
@@ -1,24 +1,40 @@
 
 
-class Filme :
+class Programa :
+    def __init__(self, nome, ano) -> None:
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self) :
+        return self._likes
+
+    def dar_like(self) :
+        self._likes += 1
+
+    @property
+    def nome(self) :
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome) :
+        self._nome = novo_nome.title()
+
+class Filme(Programa) :
     def __init__(self, nome, ano, duracao) -> None:
-        self.nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self._likes = 0
 
-    def dar_like(self) :
-        self.likes += 1
 
-class Serie :
+class Serie(Programa) :
     def __init__(self, nome, ano, temporadas) -> None:
-        self.nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
-
-    def dar_like(self) :
-        self.likes += 1
+        self._likes = 0
 
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
@@ -28,6 +44,8 @@ print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores
 
 atlanta = Serie("atlanta", 2018, 2)
 atlanta.dar_like()
+atlanta.nome = "atlanta - a terra esquecida"
 
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
 
