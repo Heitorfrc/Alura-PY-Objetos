67c3ea3a9a2af34cd0736f324133947d94e071c7
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-10-11 11:23:11 -0300

    Update Filme_Serie.py

--- Filme_Serie.py
+++ Filme_Serie.py
@@ -20,31 +20,35 @@ class Programa :
     @nome.setter
     def nome(self, novo_nome) :
         self._nome = novo_nome.title()
+    
+    def __str__(self) :
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
 class Filme(Programa) :
     def __init__(self, nome, ano, duracao) -> None:
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self) :
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 class Serie(Programa) :
     def __init__(self, nome, ano, temporadas) -> None:
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    
+    def __str__(self) :
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
         
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
 
-print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}: {vingadores.likes}')
-
 atlanta = Serie("atlanta", 2018, 2)
 atlanta.dar_like()
 atlanta.nome = "atlanta - a terra esquecida"
 
-print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas}: {atlanta.likes}')
-
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series :
-    detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
-    print(f'{programa.nome} - {programa.ano} - {detalhes} : {programa.likes}')
+    print(programa)
